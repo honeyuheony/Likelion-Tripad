c641d75faae3763cfc7afe98fa254078adf3a1e8
Psunmin <bbu0330@gmail.com>
2021-07-03 13:59:01 +0900

    tripad

--- tripad/tripad/urls.py
+++ tripad/tripad/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main import views 
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.main, name="main"),
+    path('register/',include('register.urls')),
 ]
