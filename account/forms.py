35fa642e2f3bc4914c9c2710ca01cdb51ef59fe2
Ha_hyo <32561305+ash1313@users.noreply.github.com>
2021-07-03 19:53:12 +0900

    Delete account directory

--- account/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.contrib.auth.models import User
-from .models import CustomUser
-from django.contrib.auth.forms import UserCreationForm
-
-class RegisterForm(UserCreationForm):
-    class Meta:
-        model= CustomUser
-        fields=['username','password1', 'password2']
